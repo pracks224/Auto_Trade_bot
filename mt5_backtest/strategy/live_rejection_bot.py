e95920d3eb5984ddbaf3725af568b9055572d05d
pcs <p.sh.433@gmail.com>
2026-03-24 20:27:18 +0300

    lots updated

--- mt5_backtest/strategy/live_rejection_bot.py
+++ mt5_backtest/strategy/live_rejection_bot.py
@@ -273,7 +273,7 @@ while True:
 
             # 2. Trade Management
             set_break_even(sym, atr_v)
-            //update_trailing_stop(sym, atr_v)
+            #update_trailing_stop(sym, atr_v)
 
             # 3. New Entry Logic
             open_pos = mt5.positions_get(symbol=sym, magic=MAGIC_NUMBER)
@@ -293,13 +293,13 @@ while True:
                 # BEARISH BREAKOUT: EMA Gap is huge + we broke the previous Low
                 elif last['ema9'] < last['ema200'] and ema_gap > TREND_GAP_MIN:
                     if last['close'] < prev['low']:
-                        small_lot = 0.1
+                        small_lot = 0.25
                         logger.info(f"INTENSE BEARISH: Gap {ema_gap:.2f} | Breaking Low {prev['low']}")
                         place_trade(sym, "SELL", small_lot, last['close'], atr_v, tp_multiplier=QUICK_TP_MULT)
                 # BULLISH BREAKOUT: EMA Gap is huge + we broke the previous High
                 elif last['ema9'] > last['ema200'] and ema_gap > TREND_GAP_MIN:
                     if last['close'] > prev['high']:
-                        small_lot = 0.1
+                        small_lot = 0.25
                         logger.info(f" INTENSE BULLISH: Gap {ema_gap:.2f} | Breaking High {prev['high']}")
                         place_trade(sym, "BUY", small_lot, last['close'], atr_v, tp_multiplier=QUICK_TP_MULT)
 
